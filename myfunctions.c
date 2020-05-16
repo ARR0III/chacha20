12e6c4baec26a64476e9374939f4f2ef536bd31e
ARR0III <36902548+ARR0III@users.noreply.github.com>
2020-05-16 15:04:27 +0700

    Add files via upload

---
+++ myfunctions.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int genrand(const int min, const int max) {
+  return min + rand() % ((max + 1) - min);
+}
+
+void vigenere (uint8_t * data, const int length_data, const uint8_t * key, const int length_key) {
+  int i, j;
+  
+  for (i = 0, j = 0; i < length_data; i++) {
+    data[i] ^= key[j];
+    
+    if (j == 31)
+      j = 0;
+    else
+      j++;
+  }
+}
+
+int readfromfile(const char * filename, uint8_t * buffer, const int length) {
+  FILE * f = fopen(filename, "rb");
+
+  if (f == NULL)
+    return -1;
+  
+  int result = (int)fread(buffer, 1, length, f);
+  fclose(f);
+  
+  return result;
+}
+
+void strxor (uint8_t * one, const uint8_t * two, int length) {
+  for (int i = 0; i < length; i++) {
+    one[i] ^= two[i];
+  }
+}
+
+void printhex(const int tumbler, const uint8_t * data, const int length) {
+  for (int i = 0; i < length; i++) {
+    if (tumbler)
+      printf("%02X%c", data[i], (i + 1) % 16 ? ' ' : '\n');
+    else
+      printf("%02X", data[i]);
+  }
+  putc('\n', stdout);
+}
---
+++ myfunctions.h
@@ -0,0 +1,7 @@
+#include "myfunctions.c"
+
+int  genrand(const int min, const int max);
+void vigenere (uint8_t * data, const int length_data, const uint8_t * key, const int length_key);
+int  readfromfile(const char * filename, uint8_t * buffer, const int length);
+void strxor (uint8_t * one, const uint8_t * two, const int length);
+void printhex(const int tumbler, const uint8_t * data, const int length);
